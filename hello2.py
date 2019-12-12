f16ffce77766db404faf7b1dd0746182b54fada2
namonajem <46903294+namonajem@users.noreply.github.com>
2019-12-12 00:01:05 +0000

    Add files via upload

---
+++ hello2.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+ return "The default, 'root' route"
+
+@app.route("/hello/")
+def hello():
+	return "Hello Napier!!! :D"
+
+@app.route("/goodbye/")
+def goodbye():
+	return "Goodbye cruel world :("
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0')
+
+
